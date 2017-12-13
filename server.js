575fc9ae6487b63da56bfa35c3fe8194cbce8005
Rene Rautenbach <rene@clickasite.co.za>
2017-12-13 08:38:10 +0200

    app.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 import compression from 'compression';
 
 const DIST_DIR = path.join(__dirname,'public');
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000
 const app = express();
     
 app.use(express.static(DIST_DIR)) 
@@ -17,8 +17,7 @@ app.get('/', function(req, res) {
 });  
 
 const server = http.createServer(app);
-server.listen(port, function listening() {
-  console.log('Listening on %d', server.address().port); 
+server.listen(PORT, function listening() {
+  console.log('Listening on %d', server.address().PORT); 
   websocket(server); 
-});
-
+}); 
